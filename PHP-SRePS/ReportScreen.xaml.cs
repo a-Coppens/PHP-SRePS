2eab5f707f56472b7d78fb5596f0f802ce5c2bdb
Shaun Gittins <101944934@student.swin.edu.au>
2019-10-25 19:27:27 +1100

    Set up report screen SQL access

--- PHP-SRePS/ReportScreen.xaml.cs
+++ PHP-SRePS/ReportScreen.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,39 @@ namespace PHP_SRePS
         public ReportScreen()
         {
             InitializeComponent();
+            LoadSalesToDataGrid();
+        }
+
+        public void LoadSalesToDataGrid()
+        {
+            int dayRange = 0;
+            if (pastSalesRange.SelectedItem.ToString() == "Past week sales")
+            {
+                dayRange = -7;
+            }
+            else if (pastSalesRange.SelectedItem.ToString() == "Past month sales")
+            {
+                dayRange = -28;
+            }
+
+            List<SqlParameter> sqlParameters = new List<SqlParameter>()
+            {
+                new SqlParameter() {ParameterName = "@todayDate", Value = DateTime.Today.ToString()},
+                new SqlParameter() {ParameterName = "@targetPreviousDate", Value = DateTime.Today.AddDays(dayRange).ToString()}
+            };
+
+            SqlAccessor.Open();
+            using (SqlDataReader reader = SqlAccessor.RunQuery("SELECT productID, SUM(salesQuantity) FROM Sales WHERE saleDate between '@todayDate' and '@targetPreviousDate' GROUP BY productID;", sqlParameters))
+            {
+                if (reader != null)
+                {
+                    while (reader.Read())
+                    {
+                        // TODO: Needs to set datagrid
+                    }
+                }
+            }
+            SqlAccessor.Close();
         }
     }
 }
