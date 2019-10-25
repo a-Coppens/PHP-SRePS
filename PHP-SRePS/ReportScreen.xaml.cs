5db21fef51c2a92bff3006ed40aa2b052e1d0d07
Shaun Gittins <101944934@student.swin.edu.au>
2019-10-25 23:46:05 +1100

    Weekly / Monthly report page now loads data from database

--- PHP-SRePS/ReportScreen.xaml.cs
+++ PHP-SRePS/ReportScreen.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PHP_SRePS
 {
@@ -21,6 +9,8 @@ namespace PHP_SRePS
     /// </summary>
     public partial class ReportScreen : UserControl
     {
+        private readonly srepsDatabase data = new srepsDatabase();
+
         public ReportScreen()
         {
             InitializeComponent();
@@ -29,35 +19,38 @@ namespace PHP_SRePS
 
         public void LoadSalesToDataGrid()
         {
-            int dayRange = 0;
-            if (pastSalesRange.SelectedItem.ToString() == "Past week sales")
+            // TODO: There is a UI delay / bug here, the pastSalesRange.Text does not update until it is changed a second time
+            int dayRange = -1;
+            if (pastSalesRange.Text == "Past week sales")
             {
+                Console.WriteLine("-7");
                 dayRange = -7;
             }
-            else if (pastSalesRange.SelectedItem.ToString() == "Past month sales")
+            else if (pastSalesRange.Text == "Past month sales")
             {
+                Console.WriteLine("-28");
                 dayRange = -28;
+            } 
+            else
+            {
+                Console.WriteLine("-1");
             }
 
-            List<SqlParameter> sqlParameters = new List<SqlParameter>()
-            {
-                new SqlParameter() {ParameterName = "@todayDate", Value = DateTime.Today.ToString()},
-                new SqlParameter() {ParameterName = "@targetPreviousDate", Value = DateTime.Today.AddDays(dayRange).ToString()}
-            };
+            DateTime backDate = DateTime.Today.AddDays(dayRange);
 
-            SqlAccessor.Open();
-            using (SqlDataReader reader = SqlAccessor.RunQuery("SELECT productID, SUM(salesQuantity) FROM Sales WHERE saleDate between '@todayDate' and '@targetPreviousDate' GROUP BY productID;", sqlParameters))
-            {
-                if (reader != null)
-                {
-                  /*  while (reader.Read())
-                    {
-                        // TODO: Needs to set datagrid
-                    }
-                    */
-                }
-            }
-            SqlAccessor.Close();
+            // "SELECT productID, SUM(salesQuantity) FROM Sales WHERE saleDate between '@todayDate' and '@targetPreviousDate' GROUP BY productID;"
+            var query = data.Sales
+            .Where(d => d.saleDate <= DateTime.Today && d.saleDate > backDate)
+            .GroupBy(a => a.productID)
+            .Select(a => new { TotalSales = a.Sum(b => b.salesQuantity), ProductName = a.Key })
+            .OrderByDescending(a => a.TotalSales);
+
+            if (reportDatagrid != null) reportDatagrid.ItemsSource = query.ToList();
+        }
+
+        private void pastSalesRange_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LoadSalesToDataGrid();
         }
     }
 }
